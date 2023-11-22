bd7e4311541901daa5f16652f7776f8506cc5ab2
Fitat Suleymanzade <146056401+FitatSuleymanzade@users.noreply.github.com>
2023-11-22 09:38:31 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const name = document.getElementById('Name')
+const surname = document.getElementById('Surname')
+const age = document.getElementById('Age')
+const studentID = document.getElementById('StudentID')
+const formbutton = document.getElementById('formbutton')
+const avatarDiv = document.querySelector('.avatar')
+const formid = document.getElementById('formid')
+
+axios.get("https://655c30a1ab37729791aa03c7.mockapi.io/sdu/students")
+.then((res) => {
+    const students = res.data;
+    const avatarUrl = students[1].avatar;
+    const avatarImage = document.createElement('img');
+    avatarImage.src = avatarUrl;
+    avatarDiv.appendChild(avatarImage);
+    });
+
+
+    formid.addEventListener("submit", function(event){
+        event.preventDefault();
+        axios.post("https://655c30a1ab37729791aa03c7.mockapi.io/sdu/students",{
+        name: name.value,
+        surname: surname.value,
+        age: age.value,
+        studentID: studentID.value
+     
+})
+    })
